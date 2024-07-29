b014e0c490f46880fbbb1c977f5c2a905e3d808f
madhu <madhu@Madhu>
2024-07-29 23:47:44 +0530

    new

--- src/Login.java
+++ src/Login.java
@@ -18,7 +18,9 @@ public class Login {
 		driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		jse.executeScript("window.scrollBy(0,500)");
-		jse.executeScript("window.scrollBy(0,600)");
+		jse.executeScript("window.scrollBy(0,700)");
+		
+		
 	
 		
 		
