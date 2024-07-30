355f8208d53734bc1eb601c4f14b523ef88fc84f
MadhuC-QA <madhuramkrishna10@gmailcom>
2024-07-30 12:35:52 +0530

    Added

--- src/Login.java
+++ src/Login.java
@@ -1,9 +1,16 @@
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 public class Login {
 
 	public static void main(String[] args) {
@@ -12,6 +19,7 @@ public class Login {
 		WebDriver driver =new EdgeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
 		
 		//driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Mobiles");
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Mobiles");
@@ -19,7 +27,23 @@ public class Login {
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		jse.executeScript("window.scrollBy(0,500)");
 		driver.findElement(By.linkText("Redmi Note 13 Pro (Midnight Black, 8GB RAM, 128GB Storage) | 1.5K AMOLED | 200MP Hi-Res Camera | Flagship 4nm SD 7s Gen 2 | 67W TurboCharge")).click();
+		//driver.findElement(By.xpath("//div[@class='a-section a-spacing-none a-padding-none']//input[@id='add-to-cart-button']")).click();
+		////input[@aria-labelledby='attach-sidesheet-checkout-button-announce']
 		
+	WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+	//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@aria-labelledby='attach-sidesheet-checkout-button-announce'")));
+	Set<String> windows=driver.getWindowHandles();
+	
+	Iterator<String> it=windows.iterator();
+	
+	String parentId=it.next();
+	String childId=it.next();
+	driver.switchTo().frame(1);
+	
+	
+	
+	jse.executeScript("window.scrollBy(0,500)");
+		driver.findElement(By.xpath("//div[@id='addToCart_feature_div']")).click();
 		
 		
 	
