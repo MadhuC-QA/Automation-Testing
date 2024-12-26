aa6c8ae0a0ca51fb26d5b27a283322630a83c711
MadhuC-QA <madhuramkrishna10@gmailcom>
2024-12-26 19:23:54 +0530

    added on 27 nov

---
+++ src/count_words.java
@@ -0,0 +1,27 @@
+
+public class count_words {
+
+	public static void main(String[] args) {
+		String one="AA bbb cccc";
+		
+		one=one.replaceAll("\\s", "");
+		
+		System.out.println(one);
+		
+		char[] arr=one.toCharArray();
+		for(int i=0;i<arr.length;i++)
+		{
+			int C=1;
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					C++;
+				}
+			}
+			System.out.println(arr[i] +" is "+ C +"times");
+		}
+
+	}
+
+}
